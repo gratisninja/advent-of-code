716bedf0df4584ea4b92f08a5e66b22769dc3942
gratisninja <44645382+gratisninja@users.noreply.github.com>
2018-12-20 10:04:02 +0100

    Update and rename 2016/1.py to 2016/1/1.py

---
+++ 2016/1/1.py
@@ -0,0 +1,11 @@
+a = open('input.txt', 'r').read().replace(' ', '')
+arr = [0,0,0,0]
+way = 0
+for i in a.split(','):
+  print(way)
+  way = way + 1 if i[0] == 'R' else way - 1
+  if way == 4 or way == -4:
+    way = 0
+  arr[way] += int(i[1:])
+print(arr)
+print(abs(arr[0]-arr[2]) + abs(arr[1] - arr[3]))
