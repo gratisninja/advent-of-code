ccd479504a8d841a2c04a5a7ef7dec45490e4b01
gratisninja <44645382+gratisninja@users.noreply.github.com>
2019-01-25 13:33:15 +0100

    Create main.py

---
+++ 2016/6/main.py
@@ -0,0 +1,10 @@
+messages = [line.strip() for line in open("D:/Ausbildungsinhalte/Python/AoC/2016/6_input.txt", "r")]
+pos = [{} for a in messages[0]]
+for message in messages:
+    for i, a in enumerate(message):
+        if a in pos[i]:
+            pos[i][a] += 1
+        else:
+            pos[i][a] = 1
+print('Part 1: ' + ''.join([max(a, key=a.get) for a in pos]))
+print('Part 2: ' + ''.join([min(a, key=a.get) for a in pos]))
